cdb5b8c42a0097167ef2adf9afb8efa629706dcc
Srimulyaniadha <srimulyaniadha2004@gmail.com>
2024-10-08 09:42:00 +0700

    first commit

---
+++ app/src/main/java/srimulyaniadha/com/example/androidkotlinapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package srimulyaniadha.com.example.androidkotlinapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import srimulyaniadha.com.example.androidkotlinapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvCounter.text = "1"
+
+    }
+    fun addNumber(v: View) {
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        binding.tvCounter.text = nextVal.toString()
+
+    }
+    fun resetNumber(v: View) {
+        binding.tvCounter.text = "0"
+    }
+
+    fun subtractNumber(v: View){
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        binding.tvCounter.text = nextVal.toString()
+    }
+
+}
